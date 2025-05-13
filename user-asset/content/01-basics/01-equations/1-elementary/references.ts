73ccc32fa8679ceaab48708b9031bdf35d2e45e6
Gwynerva <cmtvpetya@gmail.com>
2025-05-13 14:42:10 +0000

    Deploying to gh-pages from @ open-math/ru.omath.net@b9dce82c080efe8a710419a9acf1b3561618c77b 🚀

---
+++ user-asset/content/01-basics/01-equations/1-elementary/references.ts
@@ -0,0 +1,20 @@
+import { wikipedia } from '#content/01-basics/sources';
+
+export default defineContentReferences([
+    {
+        source: wikipedia,
+        references: [
+            {
+                title: 'Robert Recorde',
+                description: 'Английский математик и врач',
+                link: 'https://w.wiki/DhMN',
+            },
+            {
+                title: '"Утиный тест"',
+                description:
+                    'Не такой уж и безобидный "шуточный" тест на очевидность происходящего',
+                link: 'https://w.wiki/DwLz',
+            },
+        ],
+    },
+]);
---
+++ user-asset/content/01-basics/01-equations/group.ts
@@ -0,0 +1,4 @@
+export default defineGroup({
+    type: 'separator',
+    title: 'Уравнения',
+});
