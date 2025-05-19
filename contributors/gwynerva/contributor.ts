47281e664bf326504c2eacd3a9fe7270418eafce
Peter Radko <cmtvpetya@gmail.com>
2025-05-19 08:38:17 +0300

    Обновление до Erudit 3.0.0-dev.19 и больше информации об авторах

--- contributors/gwynerva/contributor.ts
+++ contributors/gwynerva/contributor.ts
@@ -1,3 +1,11 @@
 export default defineContributor({
+    editor: true,
     displayName: 'Петр Радько',
+    slogan: 'Гениями не рождаются — ими становятся!',
+    links: {
+        'Мой сайт': 'https://radkopeter.ru/',
+        GitHub: 'https://github.com/Gwynerva',
+        Telegram: 'https://t.me/math_head',
+        Почта: 'mailto:newpetya@inbox.ru',
+    },
 });
--- contributors/nagibin/contributor.ts
+++ contributors/nagibin/contributor.ts
@@ -1,3 +1,8 @@
 export default defineContributor({
+    editor: true,
     displayName: 'Николай Нагибин',
+    slogan: 'Тебе уже 20? На старт!',
+    links: {
+        VK: 'https://vk.com/nagibin2509',
+    },
 });
